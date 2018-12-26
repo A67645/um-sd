79ad1b08d7bb7c3db1d1db30b10d00f41501b398
guilhermemiranda0 <37369034+guilhermemiranda0@users.noreply.github.com>
2018-12-26 18:05:27 +0000

    Locks feitos

--- MasterServer.java
+++ MasterServer.java
@@ -12,13 +12,15 @@ class User{
 	String email;
 	String password;
 	float debt;
-	List<int> userServers = new ArrayList<int>();
-	Lock l = new ReentrantLock();
+	List<int> userServers;
+	Lock l;
 	
 	public User(String email, String password){
 		this.email = email;
 		this.password = password;
 		this.debt = 0;
+		this.userServers = new ArrayList<int>();
+		this.l = new ReentrantLock();
 	}
 	
 }
@@ -32,7 +34,7 @@ class Server{
 	char inUse;
 	char type;
 	String freeCode;
-	Lock l = new ReentrantLock();
+	Lock l;
 	
 	
 	public Server(int id, String name, float price, String code, char type){
@@ -43,6 +45,21 @@ class Server{
 		this.freeCode = code;
 		this.type = type;
 		this.auctionPrice = 0;
+		this.l = new ReentrantLock();
+	}
+	
+}
+
+class Database{
+	
+	Map<String,User> users;
+	Map<int,Server> servers;
+	Lock l;
+	
+	public Database(){	
+		this.users = new HashMap<String,User>();
+		this.servers = new HashMap<int,Server>();
+		this.l = new ReentrantLock();
 	}
 	
 }
@@ -55,133 +72,132 @@ class clientHandler extends Thread {
 	PrintWriter out = new PrintWriter(cs.getOutputStream());
 	BufferedReader in = new BufferedReader(new InputStreamReader(cs.getInputStream()));	
 	
-	Map<String,User> users;
-	Map<int,Server> servers;
+	Database database;
 	
-	clientHandler(Socket cs, Map<String,User> users, Map<int,Server> servers){
+	clientHandler(Socket cs, Database database){
 		this.cs = cs;
-		this.users = users;
-		this.servers = servers;
-
+		this.database = database;
 	}
 	
-	private void registerUser(){
-		
-		try{
+	private void registerUser() throws InterruptedException{
 							
-			String m,p;
-			User aux;
+		String m,p;
+		User aux;
 		
-			out.println("What is your e-mail?/n");
-			out.flush();
+		out.println("What is your e-mail?/n");
+		out.flush();
 		
-			while(true){				
-				m = in.readLine();
-				if(m == null){ 
-					out.print("Invalid e-mail!Try again!/n");
-					out.flush();
-				}
-				else{
-					break;
-				}			
+		while(true){				
+			m = in.readLine();
+			if(m == null){ 
+				out.print("Invalid e-mail!Try again!/n");
+				out.flush();
 			}
+			else{
+				break;
+			}			
+		}
 			
-			out.println("What is your password?/n");
-			out.flush();
+		out.println("What is your password?/n");
+		out.flush();
 			
-			while(true){				
-				p = in.readLine();
-				if(p == null){ 
-					out.print("Invalid password!Try again!/n");
-					out.flush();
-				}
-				else{
-					break;
-				}			
+		while(true){				
+			p = in.readLine();
+			if(p == null){ 
+				out.print("Invalid password!Try again!/n");
+				out.flush();
 			}
+			else{
+				break;
+			}			
+		}
 			
-			aux = new user(m,p)
+		aux = new user(m,p)
 			
-			//fazer lock de users
-			
-			if(users.get(m) != null){
-				users.put(m,aux);
+		database.l.lock();
+		try{
+			if(database.users.get(m) != null){
+				database.users.put(m,aux);
 				out.println("User registered successfully./n");
 				out.flush();
 			}
 			else{
 				out.println("E-mail already exists. User not registered./n");
 				out.flush();			
-			}		
-			
-		}catch(IOException e)	
+			}
+		}
+		finally{ database.l.unlock(); }
+
 		return;		
 	}
 	
-	private void userLogIn(){
+	private void userLogIn() throws InterruptedException{
+								
+		String m,p;
+		User aux;
 		
-		try{
-						
-			String m,p;
-			User aux;
+		out.println("What is your e-mail?/n");
+		out.flush();
 		
-			out.println("What is your e-mail?/n");
-			out.flush();
-		
-			while(true){				
-				m = in.readLine();
-				if(m == null){ 
-					out.print("Invalid e-mail!Try again!/n");
-					out.flush();
-				}
-				else{
-					break;
-				}			
+		while(true){				
+			m = in.readLine();
+			if(m == null){ 
+				out.print("Invalid e-mail!Try again!/n");
+				out.flush();
 			}
+			else{
+				break;
+			}			
+		}
 			
-			//fazer lock dos users
-			
-			if(users.get(m) == null){				
+		database.l.lock();
+		try{
+			if(database.users.get(m) == null){				
 				out.print("No user exists with that e-mail./n");
 				out.flush();				
 				return;			
 			}
+		}
+		finally{ database.l.unlock(); }
 			
-			out.println("What is your password?/n");
-			out.flush();
+		out.println("What is your password?/n");
+		out.flush();
 			
-			while(true){				
-				p = in.readLine();
-				if(p == null){ 
-					out.print("Invalid password!Try again!/n");
-					out.flush();
-				}
-				else{
-					break;
-				}			
+		while(true){				
+			p = in.readLine();
+			if(p == null){ 
+				out.print("Invalid password!Try again!/n");
+				out.flush();
 			}
+			else{
+				break;
+			}			
+		}
 			
-			aux = users.get(m);
+		database.l.lock();
+		try{
+			aux = database.users.get(m);
+		}
+		finally{ database.l.unlock(); }
 			
-			//fazer lock do user
+		aux.l.lock();
 			
-			if(aux.password != m){			
-				out.print("Wrong password!/n");
-				out.flush();	
-				return;				
-			}
-			
-			currentUser = aux;
-			
-			out.print("LogIn sucessful!/n");
+		if(aux.password != m){			
+			out.print("Wrong password!/n");
 			out.flush();
-							
-		}catch(IOException e)
-		// nao fazer unlock do user, faz-se no log-out
+			aux.l.unlock();
+			return;				
+		}
+			
+		currentUser = aux;
+			
+		out.print("LogIn sucessful!/n");
+		out.flush();
+		
 		return;		
 	}
 	
-	private showServersRented(){
+	private showServersRented() throws InterruptedException{
 		
 		int id;
 		String name;
@@ -190,26 +206,28 @@ class clientHandler extends Thread {
 		float requestPrice;
 		float auctionPrice;
 		
-		//fazer lock do user
-		
 		List<int> userServers = currentUser.userServers;
 		
-		for(int i = 0; i < userServers.size(); i++){			
+		database.l.lock();
+		try{
+			for(int i = 0; i < userServers.size(); i++){			
+				
+				id = userServers.get(i);		
+				name = database.servers.get(id).name;
+				code = database.servers.get(id).freeCode;
+				type = database.servers.get(id).type;
 			
-			id = userServers.get(i);		
-			name = servers.get(id).name;
-			code = servers.get(id).freeCode;
-			type = servers.get(id).type;
-			
-			out.print("Server id = "i"; Server name = "name"; Server type = "type"; Server freeing code = "freeCode"; /n");		
+				out.print("Server id = "i"; Server name = "name"; Server type = "type"; Server freeing code = "freeCode"; /n");		
+			}
 		}
+		finally{ database.l.unlock(); }
 		
 		out.print("List End./n");
 		out.flush;
 		
 	}
 	
-	private grantServerRequest(){
+	private grantServerRequest() throws InterruptedException{
 		
 		String name;
 		char type;
@@ -217,154 +235,141 @@ class clientHandler extends Thread {
 		String s;
 		Server requestedServer;
 		
-		//fazer lock dos servers
-		
 		out.print("Pick a server from the List: /n");
 		
-		for(int id: servers.keySet()){		
-			if(servers.get(id).inUse != 'Y'){			
-				name = servers.get(id).name;
-				type = servers.get(id).type;
-				price = servers.get(id).requestPrice;
+		database.l.lock();
+		try{
+			for(int id: database.servers.keySet()){		
+				if(database.servers.get(id).inUse != 'Y'){			
+					name = database.servers.get(id).name;
+					type = database.servers.get(id).type;
+					price = database.servers.get(id).requestPrice;
 			
-				out.print("ID: "id"; Name: "name"; Type: "type"; Price: "price";\n");
+					out.print("ID: "id"; Name: "name"; Type: "type"; Price: "price";\n");
+				}
 			}
 		}
-		
-		//fazer unlock
+		finally{ database.l.lock(); }
 		
 		out.print("Type Server ID to choose./n");
 		out.flush();
 		
 		s = in.readLine();
 		
-		//fazer lock dos servidores
-		
-		requestedServer = servers.get(Integer.parseInt(s));
-		
-		//fazer unlock
-		
-		if(requestedServer == null){
+		database.l.lock();
+		try{
+			requestedServer = database.servers.get(Integer.parseInt(s));
+		}
+		finally{ database.l.unlock(); }
 			
+		if(requestedServer == null){
+				
 			out.print("A server with that ID does not exist!/n");
 			out.flush();
-			return;
-			
+			return;		
 		}
-		
-		//fazer lock do servidor requisitado
-		
-		if(requestedServer.inUse == 'Y' ){
 			
-			out.print("Server already in use!/n");
+		requestedServer.l.lock();
+		try{
+			if(requestedServer.inUse == 'Y' ){			
+				out.print("Server already in use!/n");
+				out.flush();
+				return;
+			}
+		
+			currentUser.userServers.add(requestedServer.id);
+			currentUser.debt += requestedServer.requestPrice;
+			requestedServer.inUse = 'Y';
+		
+			out.print("Server rental sucessful! Your freeing code is: "requestedServer.freeCode"/n");
 			out.flush();
-			//fazer unlock
-			return;
-			
 		}
-		
-		currentUser.userServers.add(requestedServer.id);
-		currentUser.debt += requestedServer.requestPrice;
-		requestedServer.inUse = 'Y';
-		
-		out.print("Server rental sucessful! Your freeing code is: "requestedServer.freeCode"/n");
-		out.flush();
-		//fazer unlock
+		finally{ requestedServer.l.unlock(); }
 		return;
 	}
 	
-	private auctionServer(){		
+	private auctionServer() throws InterruptedException{		
 		
 		String name;
 		char type;
 		float price;
 		String s;
 		Server requestedServer;
-		
-		//fazer lock dos servers
-		
+
 		out.print("Pick a server from the List: /n");
 		
-		for(int id: servers.keySet()){		
-			if(servers.get(id).inUse != 'Y'){			
-				name = servers.get(id).name;
-				type = servers.get(id).type;
-				price = servers.get(id).auctionPrice;
-			
-				out.print("ID: "id"; Name: "name"; Type: "type"; Current Offer: "price";\n");
+		database.l.lock();
+		try{
+			for(int id: database.servers.keySet()){		
+				if(database.servers.get(id).inUse != 'Y'){			
+					name = database.servers.get(id).name;
+					type = database.servers.get(id).type;
+					price = database.servers.get(id).auctionPrice;
+				
+					out.print("ID: "id"; Name: "name"; Type: "type"; Current Offer: "price";\n");
+				}
 			}
 		}
-		
-		//fazer unlock
+		finally{ database.l.unlock(); }
 		
 		out.print("Type Server ID to choose./n");
 		out.flush();
 		
 		s = in.readLine();
 		
-		//fazer lock dos servidores
-		
-		requestedServer = servers.get(Integer.parseInt(s));
-		
-		//fazer unlock
+		database.l.lock();
+		try{
+			requestedServer = database.servers.get(Integer.parseInt(s));
+		}
+		finally{ requestedServer.l.lock(); }
 		
 		if(requestedServer == null){
 			
 			out.print("A server with that ID does not exist!/n");
 			out.flush();
-			return;
-			
+			return;		
 		}
 		
-		//fazer lock do servidor requisitado
+		requestedServer.l.lock();
+		try{
+			if(requestedServer.inUse == 'Y' ){				
+				out.print("Server already in use!/n");
+				out.flush();
+				return;	
+			}
 		
-		if(requestedServer.inUse == 'Y' ){
-			
-			out.print("Server already in use!/n");
+			out.print("What is your price offer?/n");
 			out.flush();
-			//fazer unlock
-			return;
-			
-		}
 		
-		out.print("What is your price offer?/n");
-		out.flush();
+			s = in.readLine();
 		
-		s = in.readLine();
+			if(Float.parseFloat(s) <= requestedServer.auctionPrice){				
+				out.print("Your offer does not beat the current offer!/n");
+				out.flush();				
+				return;			
+			}
 		
-		if(Float.parseFloat(s) <= requestedServer.auctionPrice){
-			
-			out.print("Your offer does not beat the current offer!/n");
+			if(Float.parseFloat(s) >= requestedServer.requestPrice){			
+				out.print("Your offer is equal or better to the server's request price! Use the request server option instead!/n");
+				out.flush();
+				return;
+			}
+		
+		
+			currentUser.userServers.add(requestedServer.id);
+			requestedServer.auctionPrice = Float.parseFloat(s);
+			currentUser.debt += requestedServer.auctionPrice;
+			requestedServer.inUse = 'A';
+		
+			out.print("Server rental sucessful! Your freeing code is: "requestedServer.freeCode"/n");
 			out.flush();
-			//fazer unlock
-			return;
-			
 		}
+		finally{ requestedServer.l.unlock(); }
 		
-		if(Float.parseFloat(s) >= requestedServer.requestPrice){
-			
-			out.print("Your offer is equal or better to the server's request price! Use the request server option instead!/n");
-			out.flush();
-			//fazer unlock
-			return;
-			
-		}
-		
-		
-		currentUser.userServers.add(requestedServer.id);
-		requestedServer.auctionPrice = Float.parseFloat(s);
-		currentUser.debt += requestedServer.auctionPrice;
-		requestedServer.inUse = 'A';
-		
-		out.print("Server rental sucessful! Your freeing code is: "requestedServer.freeCode"/n");
-		out.flush();
-		//fazer unlock
-		return;
-		
-		
+		return;		
 	}
 	
-	private freeServer(){
+	private freeServer() throws InterruptedException{
 		
 		String s;
 		int id;
@@ -382,22 +387,26 @@ class clientHandler extends Thread {
 		for(int i = 0; i < userServers.size(); i++){
 			
 			id = userServers.get(i);
-			serv = servers.get(id);
-			
-			if(s.equals(serv.freeCode)){
-				
-				serv.inUse = 'N';
-				userServers.remove(i);
-				
-				out.print("Server successfully freed./n");
-				out.flush();
-				//fazer unlock
-				return;
+			database.l.lock();
+			try{
+				serv = database.servers.get(id);
 			}
+			finally{ database.l.unlock(); }
 			
+			serv.l.lock();
+			try{
+				if(s.equals(serv.freeCode)){
+					
+					serv.inUse = 'N';
+					userServers.remove(i);
+					
+					out.print("Server successfully freed./n");
+					out.flush();
+					return;
+				}
+			}
+			finally{ serv.l.unlock(); }			
 		}
-		
-		//fazer unlock
 		
 		out.print("You not currently renting a server with the given freeing code./n");
 		out.flush();
@@ -513,30 +522,29 @@ class clientHandler extends Thread {
 
 class MasterServer {
 	
-	Map<String,User> users = new HashMap<String,User>();
-	Map<int,Server> servers = new HashMap<int,Server>();
+	Database database;
 
-	
 	private void init(){		
 	
+		database = new Database();
 		Server serv = new Server(1,"calc64",20.5,"HYTRD",'A'); 
-		servers.put(1,serv);
+		database.servers.put(1,serv);
 		serv = new Server(2,"calc32",15.5,"MARYC",'A');
-		servers.put(2,serv);
+		database.servers.put(2,serv);
 		serv = new Server(3,"calc126",25.5,"JDSVV",'A');
-		servers.put(3,serv);
+		database.servers.put(3,serv);
 		serv = new Server(4,"game1",33.8,"HSNBM",'B');
-		servers.put(4,serv);
+		database.servers.put(4,serv);
 		serv = new Server(5,"game2",33.8,"WQRXB",'B');
-		servers.put(5,serv);
+		database.servers.put(5,serv);
 		serv = new Server(6,"game3",33.8,"WAPMM",'B');
-		servers.put(6,serv);
+		database.servers.put(6,serv);
 		serv = new Server(7,"database200",20.0,"PBNDY",'C');
-		servers.put(7,serv);
+		database.servers.put(7,serv);
 		serv = new Server(8,"database400",30.0,"FZUTD",'C');
-		servers.put(8,serv);
+		database.servers.put(8,serv);
 		serv = new Server(9,"database600",40.0,"HVYWA",'C');
-		servers.put(8,serv);
+		database.servers.put(8,serv);
 		
 		return;
 		
@@ -551,7 +559,7 @@ class MasterServer {
 			
 		while(true){			
 			Socket cs = ss.accept();
-			new ClientHandler(cs).start();		
+			new ClientHandler(cs,database).start();		
 		}		
 	}
 	
